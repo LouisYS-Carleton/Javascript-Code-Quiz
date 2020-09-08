0d1b7a38c34a320bfdbe902fa972c6b027bc9db5
Louis Yacksmith <louisyscarleton@gmail.com>
2020-09-08 16:38:40 -0400

    Fixed restart button

--- Javascript/quiz-assignment.js
+++ Javascript/quiz-assignment.js
@@ -77,7 +77,7 @@ function StartDownCounting() {
         if (quizTime < 0) {
             clearInterval(interval);
             alert("Time's up, try again")
-            location.href = '../index.html';
+            location.href = 'index.html';
             
         }
     }, 1000);
